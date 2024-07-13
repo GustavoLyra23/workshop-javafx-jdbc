b1f46c17ec21e23ae285f4b27fe0020369834c8a
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-13 02:40:33 -0300

    main view desing

--- src/main/java/org/example/jdbcjavafx/HelloApplication.java
+++ src/main/java/org/example/jdbcjavafx/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.jdbcjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
